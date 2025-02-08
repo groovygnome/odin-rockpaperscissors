84d5754c20c49b1e475372bfcb25fe886821ebbf
groovygnome <jrp810@proton.me>
2025-02-08 10:44:00 -0500

    Finish playRound function, Implement and finish playGame function

--- rps.js
+++ rps.js
@@ -12,16 +12,38 @@ function getComputerChoice() {
 function getHumanChoice() {
     let turn = prompt("Choose rock, paper, or scissors");
     turn = turn.toLowerCase();
+    return turn;
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-
+    if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")) {
+        console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+        humanScore++;
+    } else if ((computerChoice == "rock" && humanChoice == "scissors") || (computerChoice == "paper" && humanChoice == "rock") || (computerChoice == "scissors" && humanChoice == "paper")) {
+        console.log("You lose. " + computerChoice + " beats " + humanChoice + ".");
+        computerScore++;
+    } else if (computerChoice == humanChoice) {
+        console.log("You tied!");
+    }
 }
 
-const human = getComputerChoice()
-const cpu = getHumanChoice();
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const human = getComputerChoice()
+        const cpu = getHumanChoice();
 
-playRound(human, cpu);
+        playRound(human, cpu);
+    }
+    if (humanScore > computerScore) {
+        console.log("You won the game!");
+    } else if (computerScore > humanScore) {
+        console.log("You lost the game.");
+    } else {
+        console.log("You tied!");
+    }
+}
+
+playGame();
