6d216b02c956c6f5b43d63dfa0e9d20a1093eb08
groovygnome <johnrperez810@gmail.com>
2025-02-15 19:08:18 -0500

    finish imnplementing UI, button press plays rps game, first to 5 wins

--- rps.js
+++ rps.js
@@ -17,19 +17,45 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+const results = document.createElement("div");
+document.body.appendChild(results);
 
 function playRound(humanChoice, computerChoice) {
     if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")) {
-        console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
         humanScore++;
+        let result = document.createElement("p");
+        result.textContent = "You win! " + humanChoice + " beats " + computerChoice + ". Human: " + humanScore + " Bot: " + computerScore;
+        results.appendChild(result);
     } else if ((computerChoice == "rock" && humanChoice == "scissors") || (computerChoice == "paper" && humanChoice == "rock") || (computerChoice == "scissors" && humanChoice == "paper")) {
-        console.log("You lose. " + computerChoice + " beats " + humanChoice + ".");
         computerScore++;
+        let result = document.createElement("p");
+        result.textContent = "You lose. " + computerChoice + " beats " + humanChoice + ". Human: " + humanScore + " Bot: " + computerScore;
+        results.appendChild(result);
     } else if (computerChoice == humanChoice) {
-        console.log("You tied!");
+        let result = document.createElement("p");
+        result.textContent = "You tied! Human: " + humanScore + " Bot: " + computerScore;
+        results.appendChild(result)
+    }
+
+    if (humanScore == 5) {
+        let result = document.createElement("p");
+        result.textContent = "The Human wins!";
+        results.appendChild(result);
+        humanScore = 0;
+        computerScore = 0;
+    } else if (computerScore == 5) {
+        let result = document.creatElement("p");
+        result.textContent = "The Bot wins!";
+        results.appendChild(result);
+        humanScore = 0;
+        computerScore = 0;
     }
 }
 
 let rockBtn = document.querySelector("#rock");
-let paperBtn = document.querySelector("#rock");
-let scissorsBtn = document.querySelector("#rock");
+let paperBtn = document.querySelector("#paper");
+let scissorsBtn = document.querySelector("#scissors");
+
+rockBtn.addEventListener("click", () => { playRound("rock", getComputerChoice()); });
+paperBtn.addEventListener("click", () => { playRound("paper", getComputerChoice()); });
+scissorsBtn.addEventListener("click", () => { playRound("scissors", getComputerChoice()); })
